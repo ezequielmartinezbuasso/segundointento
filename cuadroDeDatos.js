bea6415a0f160bdcb3871e15d06d59d4abef99b9
Ezequiel Martinez Buasso <estadisticasdevoley@gmail.com>
2019-06-11 12:35:56 +0200

    Cuadro tabla multiplicar

---
+++ cuadroDeDatos.js
@@ -0,0 +1,15 @@
+let numeros = [1, 2, 3, 5, 17, 29, 4, 6];
+
+for (let i = 1; i <=5 ; i++) {
+
+    let renglon = '';
+    
+    for (let j = 1; j <=5 ; j++){
+
+        renglon += (j.toString()*i) + ";";
+    }
+    
+    console.log(renglon);
+    }
+   
+
