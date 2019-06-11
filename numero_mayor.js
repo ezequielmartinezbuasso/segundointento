456053b1160264b325454458601f4655ed0943ae
Ezequiel Martinez Buasso <estadisticasdevoley@gmail.com>
2019-06-11 10:56:32 +0200

    numero mayor

---
+++ numero_mayor.js
@@ -0,0 +1,11 @@
+let numeros = [1,2,3,5,17,29,4,6];
+let mayor = -10;
+
+
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros [i] >= mayor) {
+    mayor = numeros [i]
+    }
+}
+    console.log(' el numero mayor es ' + mayor);
+
