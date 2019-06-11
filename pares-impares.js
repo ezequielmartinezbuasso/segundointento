25f1c997419e2f243e5d17476eb8342b4fa58a45
Ezequiel Martinez Buasso <estadisticasdevoley@gmail.com>
2019-06-11 11:15:03 +0200

    numeros pares e impares

--- pares-impares.js
+++ pares-impares.js
@@ -1,11 +1,12 @@
-let numeros = [1,2,3,5,17,29,4,6];
+let numeros = [1, 2, 3, 5, 17, 29, 4, 6];
 
-for (let i = 0; i <= numeros.length; i++) {
-    let numero = numeros [i]
+for (let i = 0; i < numeros.length; i++) {
+
+    let numero = numeros [i];
+
     if ( (numero % 2) === 0) {
-    console.log(' el numero ' + pares + ' es un numero par');
-    }
-    else{
+    console.log(' el numero ' + numero + ' es un numero par');
+    }    else{
     console.log(' el numero ' + numero + ' es un numero impar');
     }
 }   
